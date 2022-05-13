755442ca390776191bd70e904120ec9b0e484640
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-13 20:56:43 +0600

    basic code

--- basic.py
+++ basic.py
@@ -1,1 +1,13 @@
-print("hello")
+#  first print-------
+# print("hello")
+
+# indentation ---------
+""" if 5>2:  
+ print("five is greater than two") """
+
+# variable----------
+""" 
+x=5
+y="bangladesh"
+print(x,x,y)
+ """
