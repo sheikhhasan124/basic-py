ab620e74458bd7ab012dedabd1c6f3630f0798f0
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-14 06:08:14 +0600

    number done

--- basic.py
+++ basic.py
@@ -11,5 +11,21 @@ x=5
 y="bangladesh"
 print(x,x,y)
  """
-carName = "tesla lover"
-print(carName)
+""" carName = "tesla lover"
+print(carName) """
+
+# numver ------------
+""" z= 1j
+x=1
+y=1.3
+print(type(z))
+print(type(x))
+print(type(y))
+
+
+b = float(x)
+c = complex(y)
+
+
+print(type(b))
+print(type(c)) """
