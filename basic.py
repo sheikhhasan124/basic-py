f1c51766a92210d2257028c7d5a3c531edb06c87
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-14 06:47:03 +0600

    string method done

--- basic.py
+++ basic.py
@@ -29,3 +29,31 @@ c = complex(y)
 
 print(type(b))
 print(type(c)) """
+
+# string ----------
+""" a = 'bangladesh lover'
+print(a[1]) """
+
+""" for x in a:
+    print(x) """
+    
+# print(len(a))    
+
+# print("lover" in a)
+
+""" if "lover" in a:
+    print("yes it has") """
+
+""" a = "hello"
+b = "world"
+print(a+" "+b) """
+
+""" age = 23
+desc = "im hasan im in old {}"
+print(desc.format(age)) """
+
+""" a = "hello"
+print(a[2:4]) """
+
+a= "hello, word"
+print(a.split(","))
