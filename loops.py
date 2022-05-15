cdc18d5775a7c53386144c87d5a1b5051c3c8f98
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-16 05:52:27 +0600

    for loops done

---
+++ loops.py
@@ -0,0 +1,18 @@
+#--------For Loops -----------
+""" fruits = ["apple", "banana", "cherry"]
+for n in fruits:
+    print(n) """
+
+""" for n in "banana":
+     print(n)  """  
+
+""" for n in range(5):
+    print(n) """
+
+    #----------nested loops ---------------
+""" adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for n in adj:
+       for m in fruits:
+           print(n,m)
+ """
