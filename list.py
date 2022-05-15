5c5929007b34d109770d18d8190570238ed39f72
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-15 21:53:45 +0600

    read and write list code

---
+++ list.py
@@ -0,0 +1,8 @@
+#-------List is a collection which is ordered and changeable. Allows duplicate members.---------
+#python list(like an array in js)
+""" froots = ["apple", "banana","cherry"]
+print(len(froots)) """
+
+# list() constractor
+""" createList = list(("apple","bannana","cherry"))
+print(createList) """
