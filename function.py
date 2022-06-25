0593dfb6992945c2085407cf9d8aada689d7b0c2
hasan-ullah <mdhasanullah124@gmail.com>
2022-06-25 07:44:08 +0600

    functin practice

---
+++ function.py
@@ -0,0 +1,13 @@
+def my_func():
+    print("hello")
+    
+my_func()    
+
+def my_func2(name):
+    print('hello ' + name)
+
+my_func2("hasan") 
+
+def mya_add(a, b):
+    print(a + b)
+mya_add(2,3)    
